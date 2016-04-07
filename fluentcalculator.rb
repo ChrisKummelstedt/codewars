82d099b37913b70bf235f1d6e8c0c441f92c6e87
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-04-07 15:17:51 +0200

    basic

---
+++ fluentcalculator.rb
@@ -0,0 +1,31 @@
+class Calc
+
+  def one
+    return 1
+  end
+  def two
+    return 2
+  end
+  def three
+    return 3
+  end
+  def four
+    return 4
+  end
+  def five
+    return 5
+  end
+  def six
+    return 6
+  end
+  def seven
+    return 7
+  end
+  def eight
+    return 8
+  end
+  def nine
+    return 9
+  end
+  
+end
