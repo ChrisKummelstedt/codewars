cc7292d018c476caec5566d274addb93ba7003e1
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-04-07 15:48:23 +0200

    updated with values

--- fluentcalculator.rb
+++ fluentcalculator.rb
@@ -1,31 +1,121 @@
 class Calc
 
+
+  Calc.new.one.plus.two
+
+ attr_accessor :first_value, :second_value, :operation
+
+
+  def initialize
+    @first_value = nil
+    @second_value = nil
+    @operation = ''
+  end
+
+
+  def plus
+    @operation = ''
+  end
+
+  def calc_it
+
+    if @operation == 'plus'
+
+
+  end
+
+
+
+
+  def zero
+    if @first_value == nil
+       @first_value = 0
+    else
+      @first_value = 0
+      self.calc_it
+    end
+  end
+
+
   def one
-    return 1
+    if @first_value == nil
+       @first_value = 1
+    else
+      @first_value = 1
+      self.calc_it
+    end
   end
+
   def two
-    return 2
+    if @first_value == nil
+       @first_value = 2
+    else
+      @first_value = 2
+      self.calc_it
+    end
   end
+
   def three
-    return 3
+    if @first_value == nil
+       @first_value = 3
+    else
+      @first_value = 3
+      self.calc_it
+    end
   end
+
   def four
-    return 4
+    if @first_value == nil
+       @first_value = 4
+    else
+      @first_value = 4
+      self.calc_it
+    end
   end
+
   def five
-    return 5
+    if @first_value == nil
+       @first_value = 5
+    else
+      @first_value = 5
+      self.calc_it
+    end
   end
+
   def six
-    return 6
+    if @first_value == nil
+       @first_value = 6
+    else
+      @first_value = 6
+      self.calc_it
+    end
   end
+
   def seven
-    return 7
+    if @first_value == nil
+       @first_value = 7
+    else
+      @first_value = 7
+      self.calc_it
+    end
   end
+
   def eight
-    return 8
+    if @first_value == nil
+       @first_value = 8
+    else
+      @first_value = 8
+      self.calc_it
+    end
   end
+
   def nine
-    return 9
+    if @first_value == nil
+       @first_value = 9
+    else
+      @first_value = 9
+      self.calc_it
+    end
   end
-  
+
 end
